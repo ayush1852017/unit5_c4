f07721825620997ffac65646d6a9ea03d1b8944f
Ayush Bajpai <mr.ayushbajpai185@gmail.com>
2022-05-02 16:56:47 +0530

    done

---
+++ evaluation/src/components/ProtextedRoute.jsx
@@ -0,0 +1,10 @@
+// /orders and /neworder are protected routes
+import React from "react";
+import { Navigate } from "react-router";
+export const ProtectedRoute = ({ children }) => {
+  if (!true) {
+    return <Navigate to='/login' replace={false} />;
+  }
+
+  return children;
+};
